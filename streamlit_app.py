61c98cde8c6a4331be747bf48a0290dd1874b9a9
Ahmed Jadani <ahmed4star@gmail.com>
2022-12-25 16:43:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import datetime
-import time
+import time 
 
 st.title("Current Time")
 
-while True:
-    st.write(datetime.datetime.now())
-    st.empty()
-    time.sleep(1)
-    
 st.markdown("""
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Digital-7&display=swap');
         h1 {
+            font-family: 'Digital-7', sans-serif;
             text-align: center;
             font-size: 50px;
         }
     </style>
     """, unsafe_allow_html=True)
+
+@st.cache(interval=1)
+def get_time():
+    return datetime.datetime.now().strftime('%H:%M:%S')
+
+st.write(get_time(), unsafe_allow_html=True)
