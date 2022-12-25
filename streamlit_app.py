2bf33d19e69fe3ac97f1748ed00856ffecaeca6c
Ahmed Jadani <ahmed4star@gmail.com>
2022-12-25 16:28:30 +0100

    time now

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,17 @@
-print('test')
+import streamlit as st
+import datetime
+
+st.title("Current Time")
+
+current_time = datetime.datetime.now()
+st.write(f"The current time is: {current_time.strftime('%H:%M:%S')}")
+
+st.markdown("""
+    <style>
+        h1 {
+            text-align: center;
+            font-size: 50px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
